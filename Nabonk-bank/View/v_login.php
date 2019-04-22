4212ef5ae2eb8e4f2346fd147b2fd518856f2fb2
Robertus Dwi Ari Utomo <47166058+penguin55@users.noreply.github.com>
2019-04-22 12:23:31 +0700

    First Commit

---
+++ Nabonk-bank/View/v_login.php
@@ -0,0 +1,16 @@
+<! DOCTYPE html>
+<HTML>
+    <HEAD>
+        <TITLE> Nabonk Bank Login </TITLE>
+        <a href="../index.html" name="header"><H1> Nabonk Bank Login </H1> </a>
+    </HEAD>
+    <BODY>
+        <FORM action ="" method = 'post'>
+            Username <INPUT type = 'text' placeholder="username" name="username" required>
+            <BR>
+            PIN <INPUT type = 'password' placeholder="******" name="pin" required>
+            <BR>
+            <INPUT type="submit" value="Login">
+        </FORM>
+    </BODY>
+</HTML>
---
+++ Nabonk-bank/View/v_register.php
@@ -0,0 +1,35 @@
+<! DOCTYPE html>
+<HTML>
+    <HEAD>
+        <TITLE> Nabonk Bank Registration </TITLE>
+        <a href="../index.html" name="header"><H1> Nabonk Bank Registration </H1> </a>
+    </HEAD>
+    <BODY>
+        <FORM action ="" method = 'post'>
+            Front Name <INPUT type = 'text' placeholder="Mr. Robertus" name="f-name" required>
+            <BR>
+            Last Name <INPUT type = 'text' placeholder="Ari" name="l-name" required>
+            <BR>
+            Username <INPUT type = 'text' placeholder="username" name="username" required>
+            <BR>
+            PIN <INPUT type = 'password' placeholder="******" name="pin" required>
+            <BR>
+            Confirmation PIN <INPUT type = 'password' placeholder="******" name="conf-pin" required>
+            <BR>
+            Transaction Password <INPUT type = 'password' placeholder="**********" name="trans-pin" required>
+            <BR>
+            Confirmation Transaction <INPUT type = 'password' placeholder="**********" name="conf-trans-pin" required>
+            <BR>
+            Provinsi <INPUT type = 'text' placeholder="Jawa Timur" name="prov" required>
+            Kota <INPUT type = 'text' placeholder="Malang" name="city" required>
+            <BR>
+            Alamat <INPUT type = 'text' placeholder="Jl. Watugong" name="address" required>
+            <BR>
+            Email <INPUT type = 'text' placeholder="robert@gmail.com" name="email" required>
+            <BR>
+            Telephone <INPUT type = 'number' placeholder="082254923023" name="telephone" required>
+            <BR>
+            <INPUT type="submit" value="Register">
+        </FORM>
+    </BODY>
+</HTML>
