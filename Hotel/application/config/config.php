1f4486791efe0aed66b0e02bb5f959167185d5f9
newafgani97 <newafgani97@gmail.com>
2019-11-19 12:43:19 +0700

    config

--- Hotel/application/config/config.php
+++ Hotel/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://reito/ProjectAkhir/Hotel/';
+$config['base_url'] = 'http://http://localhost/pemweb/hotelPemweb/Hotel/';
 
 /*
 |--------------------------------------------------------------------------
